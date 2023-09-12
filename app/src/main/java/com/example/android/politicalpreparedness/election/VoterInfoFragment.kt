78952ee1aa3354debf6c8e84d36792ddda08bfc8
Utkarsh Karki <utkarsh1998@icloud.com>
2023-09-12 09:50:59 -0700

    follow/unfollow fixing

--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -16,6 +16,7 @@ import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.android.politicalpreparedness.BuildConfig
--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
@@ -60,12 +60,6 @@ class VoterInfoViewModel(
         }
     }
 
-    //TODO: Add live data to hold voter info
-
-    //TODO: Add var and methods to populate voter info
-
-    //TODO: Add var and methods to support loading URLs
-
     //TODO: Add var and methods to save and remove elections to local database
     //TODO: cont'd -- Populate initial state of save button to reflect proper action based on election saved status
 
